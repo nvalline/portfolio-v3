d781e67d44299f77c5b0b518f33a9de2b1fff88a
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-07-22 18:41:16 -0600

    added switch for routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './layout/header/Header';
 import Home from './pages/Home';
@@ -13,14 +13,16 @@ import './styles/App.css';
 function App() {
   return (
     <Router>
-      <div className="main">
-        <Header />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/my-work" component={Work} />
-        <Route exact path="/contact" component={Contact} />
-        <Footer />
-      </div>
+      <Switch>
+        <div className="main">
+          <Header />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/my-work" component={Work} />
+          <Route exact path="/contact" component={Contact} />
+          <Footer />
+        </div>
+      </Switch>
     </Router>
   );
 }
